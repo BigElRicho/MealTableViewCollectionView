b222308cc01bdf8e27056bd9042ed4abace19e87
Julian Rich <s3407957@student.rmit.edu.au>
2020-09-29 19:54:41 +1000

    Initial Commit

---
+++ MealTableViewCollectionView/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MealTableViewCollectionView
+//
+//  Created by Julian Rich on 29/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MealTableViewCollectionView/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MealTableViewCollectionView
+//
+//  Created by Julian Rich on 29/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
